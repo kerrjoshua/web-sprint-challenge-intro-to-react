9660257928198a00c8ce066955ff158ba459d3b1
Joshua Kerr <kerrjoshua@gmail.com>
2023-01-11 17:20:42 +0900

    I am going to have to abandon my efforts to bring in data from the starships (and vehicles) api pages because I don't have the knowledge to do it yet and need to finish this module. Committing here to preserve the fruitless effort.

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+const  Character = props => {
+   const {name, height, mass, birth_year, eye_color, hair_color, skin_color, species, starships, vehicles} =  props.data;
+    console.log("Data: ",props.data);
+    return (
+        <div>
+            <h3>Name: {name}</h3>
+            <div>Born: {birth_year}</div>
+            <div>Height: {height}, Mass: {mass}</div>
+            <div>Species: {species}, Eyes: {eye_color}, Hair: {hair_color}, Skin: {skin_color}</div>
+            {/* <div>Starships: {starships.map(ship => {
+            return `${ship.name}, `})} </div> */}
+        </div>
+    );
+
+}
+
+export default Character;
